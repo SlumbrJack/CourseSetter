49528ef61620ebb40ff063921f74969b91b4e6ba
ndstancilFullsail <ndstancil@student.fullsail.edu>
2024-03-06 21:10:43 -0500

    Added images for nav bar and updated nav code

--- app/src/main/java/com/example/coursesetter/MainActivity.kt
+++ app/src/main/java/com/example/coursesetter/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.coursesetter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.coursesetter.R.id.navigation_dashboard
+import com.example.coursesetter.R.id.navigation_home
 import com.example.coursesetter.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
-// This is my comment (nick was here)//
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -23,13 +27,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Setup for BottomNavigationView
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
+        // Setup for (Side Navigation)
+        val drawerNavView: NavigationView = binding.sideNavigation
+        drawerNavView.setupWithNavController(navController)
+
+        // Handling navigation drawer item clicks
+        drawerNavView.setNavigationItemSelectedListener { menuItem ->
+
+            // Handle drawer item clicks here
+            when (menuItem.itemId) {
+                R.id.navigation_Home -> {
+                    // Handle navigation to the home destination
+                    navController.navigate(R.id.navigation_home)
+
+                }
+
+                R.id.navigation_dashboard ->  {
+                    // Handle navigation to the dashboard destination
+                    navController.navigate(R.id.navigation_dashboard)
+                }
+
+                R.id.navigation_settings -> {
+                    // Handle navigation to the notifications destination
+
+                }
+                // Add cases for other menu items as needed
+                else -> {
+                    // Handle default case or any other menu item not explicitly handled
+                }
+            }
+            menuItem.isChecked = true
+            binding.drawerLayout.closeDrawers()
+            true
+        }
+
+
+
+        // Setup for BottomNavigationView
+
+
         // Set of top-level destinations in the bottom nav
         val topLevelDestinations = setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,
+            navigation_home, navigation_dashboard, R.id.navigation_notifications
 
         )
 
@@ -38,20 +80,6 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        // Setup for (Side Navigation)
-        val drawerNavView: NavigationView = binding.sideNavigation
-        drawerNavView.setupWithNavController(navController)
-
-        // Handling navigation drawer item clicks
-        drawerNavView.setNavigationItemSelectedListener { menuItem ->
-            // Handle drawer item clicks here
-            when (menuItem.itemId) {
-                // Add cases for different menu items, e.g., R.id.nav_home
-            }
-            menuItem.isChecked = true
-            binding.drawerLayout.closeDrawers()
-            true
-        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
